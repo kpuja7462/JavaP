b5de79169922fa3a396a57382d4ba8e2dff55ce3
kpuja7462 <pujak24191@gmail.com>
2025-11-22 20:07:50 +0530

    wsapNo

---
+++ swapNo.java
@@ -0,0 +1,63 @@
+// public class swapNo{
+//     public static void main(String []args){
+//         int a= 10;
+//         int b =20;
+//         a = a+b;
+//         b = a-b;
+//         a = a-b;
+//         System.out.println("a:-"+" "+a);
+//         System.out.println("b:-"+" "+b);
+//     }
+// }
+
+
+
+
+// import java.util.*;
+// public class swapNo{
+//     public static void main(String []args){
+//         Scanner sc = new Scanner(System.in);
+//         int a= sc.nextInt();
+//         int b = sc.nextInt();
+//         a = a+b;
+//         b = a-b;
+//         a = a-b;
+//         System.out.println("a:-"+" "+a);
+//         System.out.println("b:-"+" "+b);
+
+//     }
+// }
+
+
+// import java.util.*;
+// public class swapNo{
+//     public static void main(String []args){
+//         Scanner sc = new Scanner(System.in);
+//         int a= sc.nextInt();
+//         int b = sc.nextInt();
+//         a = a*b;
+//         b = a/b;
+//         a = a/b;
+//         System.out.println("a:-"+" "+a);
+//         System.out.println("b:-"+" "+b);
+
+//     }
+// }
+
+
+
+
+
+
+public class swapNo{
+    public static void main(String []args){
+        int a=5;
+        int b=10;
+         a = a^b;
+         b = a^b;
+         a = a^b; 
+        System.out.println("a:-"+" "+a);
+        System.out.println("b:-"+" "+b);
+
+    }
+}
