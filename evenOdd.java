fddf5a5ca871b95ccd8e3693bb00b689f3842e63
kpuja7462 <pujak24191@gmail.com>
2025-11-22 19:04:44 +0530

    evenOdd

---
+++ evenOdd.java
@@ -0,0 +1,25 @@
+// public class evenOdd {
+//     public static void main(String []args){
+//         int num =7;
+//         if(num%2==0){
+//             System.out.println("Even");
+//         }else{
+//             System.out.println("odd");
+//         }
+//     }
+    
+// }
+
+import java.util.*;
+public class evenOdd {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+    
+}
